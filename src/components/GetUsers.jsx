cfcf9a336818662c5a1bd75e5f58390b8719f9c4
Melodi Bayraktar <101207246+MelodiBayraktar@users.noreply.github.com>
2023-08-03 22:47:33 +0300

    Users table is created

--- src/components/GetUsers.jsx
+++ src/components/GetUsers.jsx
@@ -1,6 +1,13 @@
 import { usersMockData } from "../_mock/users/";
 import React, { useEffect, useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import UsersTable from "./UsersTable";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { Paper } from "@mui/material";
+import { Visibility as VisibilityIcon } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom"; // useNavigate'i import etmeyi unutmayın
 
 function GetUsers() {
   const [users, setUsers] = useState([]);
@@ -17,21 +24,63 @@ function GetUsers() {
     }, 1000);
   };
 
+  const columns = React.useMemo(
+    () => [
+      { field: "id", headerName: "ID", width: 90 },
+      {
+        field: "name",
+        flex: 1,
+        headerName: "First name",
+      },
+      {
+        field: "lastname",
+        flex: 1,
+        headerName: "Last name",
+      },
+      {
+        field: "actions",
+        type: "actions",
+        width: 80,
+        getActions: (params) => [
+          // eslint-disable-next-line react/jsx-key
+          <GridActionsCellItem
+            label="View"
+            icon={<VisibilityIcon />}
+            onClick={() => navigateToUser(params.id)} // Burada useNavigate kullanarak yönlendirme yapın
+          />,
+        ],
+      },
+    ],
+    []
+  );
+
+  const navigate = useNavigate(); // useHistory'i useNavigate ile değiştirin
+  const navigateToUser = (id) => {
+    navigate(`/users/${id}`);
+  };
+
   return (
     <div>
       {loading ? (
         <CircularProgress
-          color="primary" // Daire rengini belirler
-          size={80} // Dairesel boyutu belirler
-          thickness={5} // Daire kalınlığını belirler
+          color="primary"
+          size={80}
+          thickness={5}
         />
       ) : (
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
+        <Paper sx={{ width: "100%" }}>
+          <DataGrid
+            rows={users.map((item) => ({
+              id: item.id,
+              name: item.name,
+              lastname: item.lastname,
+            }))}
+            columns={columns}
+            rowSelection={false}
+          />
+        </Paper>
       )}
+    
     </div>
   );
 }
