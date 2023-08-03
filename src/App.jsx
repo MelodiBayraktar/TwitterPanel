0f51e04d8e695a8632da2b1ad817324c56f6ab9d
Melodi Bayraktar <101207246+MelodiBayraktar@users.noreply.github.com>
2023-08-03 13:57:36 +0300

    react-router-dom is installed, error page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import * as React from 'react';
-import SideNav from './components/SideNav';
-import Home from './pages/Home';
-import Box from '@mui/material/Box';
+import * as React from "react";
+import SideNav from "./components/SideNav";
+import Home from "./pages/Home";
+import Box from "@mui/material/Box";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-    {/* SideNav bileşeni */}
-    <SideNav />
-    <Box component="main" sx={{ flexGrow: 1, p: 8, overflowY: 'auto' ,m:2}}>
-      {/* Diğer bileşenler */}
-     
-      <Home/>
-    </Box>
-  </Box>
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <SideNav />{" "}
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 8, overflowY: "auto", m: 2 }}
+        >
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="*" element={<ErrorPage/>} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div>
+        <h2>404 Not Found</h2>
+        <p>We are sorry, we couldn't find this page...</p>
+    </div>
+  )
+}
+export default ErrorPage;
